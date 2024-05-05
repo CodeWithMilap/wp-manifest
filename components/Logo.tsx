a04a7404dd010c55b9f6b005215f19a06458453d
CodeWithMilap <milapdave15@gmail.com>
2024-05-05 11:06:40 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,9 +5,9 @@ const Logo = () => {
     // Link to the home page with padding and a responsive logo
     <Link
       href='/'
-      className='-m-1.5  p-1.5 text-lg font-bold uppercase tracking-[0.5em]  text-black'
+      className='-m-1.5  p-1.5 text-3xl font-bold uppercase tracking-[0.1em]  text-black'
     >
-      <span className='sr-only'>Manifest</span> Manifest
+      <span className='sr-only'>Milap Dave</span> MD.
     </Link>
   );
 };
