e3a3d6f24316b9007af121fc93553676c7c20c4d
CodeWithMilap <milapdave15@gmail.com>
2024-06-18 22:17:30 +0530

    Update SocialLinks.tsx

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,14 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const SocialLinks = () => {
   return (
     <div className="overflow-hidden">
       <div className={`flex gap-4 uppercase py-4 font-semibold text-sm tracking-[0.1em] text-black`}>
-        <a className="underline underline-offset-4 hover:no-underline" href="mailto:milapdave15@gmail.com?Subject=Hello Milap | Front End Developer">Email</a>
-        <a className="underline underline-offset-4 hover:no-underline" rel="noreferrer noopener" href="https://github.com/milapdave" target="_blank">github</a>
-        <a className="underline underline-offset-4 hover:no-underline" rel="noreferrer noopener" href="https://www.linkedin.com/in/milapdave/" target="_blank">linkedin</a>
-        <a className="underline underline-offset-4 hover:no-underline" rel="noreferrer noopener" href="https://www.facebook.com/milapdave" target="_blank">facebook</a>
-        <a className="underline underline-offset-4 hover:no-underline" rel="noreferrer noopener" href="https://www.youtube.com/@milapdave" target="_blank">youtube</a>
+        <Link className="relative group hover:text-white" href="mailto:milapdave15@gmail.com?Subject=Hello Milap | Front End Developer"><span className='px-[2px]'>Email</span><span className="absolute left-0 -bottom-[0px] w-full h-[2px] transition-all bg-black -z-10 group-hover:h-full"></span></Link>
+        <Link className="relative group hover:text-white" rel="noreferrer noopener" href="https://github.com/milapdave" target="_blank"><span className='px-[2px]'>github</span><span className="absolute left-0 -bottom-[0px] w-full h-[2px] transition-all bg-black -z-10 group-hover:h-full"></span></Link>
+        <Link className="relative group hover:text-white" rel="noreferrer noopener" href="https://www.linkedin.com/in/milapdave/" target="_blank"><span className='px-[2px]'>linkedin</span><span className="absolute left-0 -bottom-[0px] w-full h-[2px] transition-all bg-black -z-10 group-hover:h-full"></span></Link>
+        <Link className="relative group hover:text-white" rel="noreferrer noopener" href="https://www.facebook.com/milapdave" target="_blank"><span className='px-[2px]'>facebook</span><span className="absolute left-0 -bottom-[0px] w-full h-[2px] transition-all bg-black -z-10 group-hover:h-full"></span></Link>
+        <Link className="relative group hover:text-white" rel="noreferrer noopener" href="https://www.youtube.com/@milapdave" target="_blank"><span className='px-[2px]'>youtube</span><span className="absolute left-0 -bottom-[0px] w-full h-[2px] transition-all bg-black -z-10 group-hover:h-full"></span></Link>
       </div>
     </div>
   )
