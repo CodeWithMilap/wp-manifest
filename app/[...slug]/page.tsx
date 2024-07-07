8a941b7f14f5d8c3af085903093f59e23716c651
CodeWithMilap <milapdave15@gmail.com>
2024-07-07 10:44:46 +0530

    Update page.tsx

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -44,3 +44,29 @@ export default async function Home({ params }: Props) {
     </>
   );
 }
+export async function generateStaticParams(): Promise<Array<object>> {
+  const preview = process.env.NEXT_PUBLIC_STORYBLOK_VERSION === 'draft';
+  const storyblokApi = getStoryblokApi();
+  const perPage = 100; // You can change this value as needed.
+  let currentPage = 1;
+  let allStories = [] as any;
+
+  while (true) {
+    const stories = await storyblokApi.get(`cdn/stories`, {
+      per_page: perPage,
+      page: currentPage,
+      version: preview ? 'draft' : 'published',
+      // excluding_slugs: "config,find-a-members/*,member-resources/*,about/*,members-only/*,news-and-events/*"
+    });
+    const currentStories = stories?.data?.stories || [];
+    if (currentStories.length === 0) {
+      break; // No more results, exit the loop.
+    }
+
+    allStories = [...allStories, ...currentStories];
+    currentPage++;
+  }
+  return allStories.map((x: any) => {
+    return { slug: x.full_slug.split('/') };
+  });
+}
